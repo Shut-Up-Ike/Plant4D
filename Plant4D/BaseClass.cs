68281c26e8139937c0fbc48237314a4a133890eb
Shut-Up-Ike <220612460+Shut-Up-Ike@users.noreply.github.com>
2025-07-17 11:26:10 -0500

    Split enums to own class

--- Plant4D/BaseClass.cs
+++ Plant4D/BaseClass.cs
@@ -2,67 +2,7 @@
 
 namespace Plant4D
 {
-    /// <summary>
-    /// Combination of enums ServerType and P4DVersion
-    /// </summary>
-    public enum ServerTypeAndP4DVersion
-    {
-        None,
-        ProductionAthena,
-        DevelopmentAthena,
-        ProductionRome,
-        DevelopmentRome
-    }
-
-    /// <summary>
-    /// Enumeration of types of servers (Production or Development)
-    /// </summary>
-    public enum ServerType
-    {
-        None,
-        Production,
-        Development
-    }
-
-    /// <summary>
-    /// Enumeration of versions of Plant-4D (currently Athena or Rome)
-    /// </summary>
-    public enum P4DVersion
-    {
-        None,
-        Athena,
-        Rome
-    }
-
-
-    /// <summary>
-    /// Struct used to pass data about projects out of PCE class
-    /// </summary>
-    public struct PCEProject
-    {
-        public string dbName;
-        public string Description { get; set; }
-        public string Year;
-        public PCEProject(string dbname, string description, string year)
-        {
-            dbName = dbname;
-            Description = description;
-            Year = year;
-        }
-        public override string ToString()
-        {
-            return Description;
-        }
-    }
-
-    internal enum P4DUserType
-    {
-        None,
-        Basic,
-        Elevated,
-        Admin
-    }
-    class BaseClass
+    internal class BaseClass
     {
         /// <summary>
         /// Get the sql connection string of the PCE database used by the supplied ServerType and P4DVersion.
